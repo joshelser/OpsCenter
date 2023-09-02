c0936b3323ebc87d6abf435929df2436fd60910d
Josh Elser <josh@sundeck.io>
2023-09-02 17:17:37 -0500

    a test

---
+++ test/unit/test_josh.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from common_utils import run_proc
+from common_utils import row_count
+from common_utils import run_sql
+import time
+
+def test_josh_runs_tests():
+    assert True
+
