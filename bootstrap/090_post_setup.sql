c89e9b99b15a966461600ab19d783399cabf52e2
Ryan Murray <rymurr@gmail.com>
2024-04-09 16:00:05 +0200

    spelling mistake

--- bootstrap/090_post_setup.sql
+++ bootstrap/090_post_setup.sql
@@ -1,4 +1,4 @@
 
 grant usage on schema analysis to application role admin;
-grant usage on procedure analysis.refresh_autorouting(varchar, varchar, object, varchar, varchar) to applpication role admin;
+grant usage on procedure analysis.refresh_autorouting(varchar, varchar, object, varchar, varchar) to application role admin;
 grant select on table analysis.autorouting_history to application role admin;
